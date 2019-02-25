9c496481169bd2d825f0a3dec76987634fb09d8d
Tamer <tamersy>
2019-02-25 16:15:23 +0200

    Easy C# app

---
+++ Square/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square
+{
+    class Program
+    {
+        static double Square(double Number)
+        {
+            return Number * Number;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number :");
+            double Num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The square of {Num} is {Square(Num)}");
+            Console.ReadKey();
+        }
+    }
+}
